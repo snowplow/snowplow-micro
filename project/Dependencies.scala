316330137743019c10fc2a7c8447c5b10b911eb0
Ian Streeter <ian@snowplowanalytics.com>
2026-04-02 16:47:34 +0100

    Bump Enrich to 6.10.0

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -17,11 +17,10 @@ object Dependencies {
   object V {
     // Snowplow
     val snowplowStreamCollector = "3.7.0"
-    val snowplowCommonEnrich    = "6.9.0"
+    val snowplowCommonEnrich    = "6.10.0"
     val snowplowAnalyticsSdk    = "3.2.0"
 
     val http4sCirce = "0.23.23"
-    val circe       = "0.14.2"
     val decline     = "2.4.1"
     val slf4j       = "2.0.17"
     val doobie      = "1.0.0-RC10"
@@ -36,9 +35,6 @@ object Dependencies {
     val gcpSdk           = "2.45.0"
     val azureStorageBlob = "12.33.2"
     val azureIdentity    = "1.18.2"
-
-    // force versions of transitive dependencies
-    val badRows   = "2.2.0"
   }
 
   val snowplowStreamCollector = "com.snowplowanalytics" %% "snowplow-stream-collector-http4s-core" % V.snowplowStreamCollector
@@ -46,13 +42,8 @@ object Dependencies {
   val snowplowAnalyticsSdk    = "com.snowplowanalytics" %% "snowplow-scala-analytics-sdk"          % V.snowplowAnalyticsSdk
   
   val http4sCirce  = "org.http4s"    %% "http4s-circe"        % V.http4sCirce
-  val http4sClient = "org.http4s"    %% "http4s-ember-client" % V.http4sCirce
   val decline      = "com.monovore"  %% "decline-effect"      % V.decline
   val slf4jSimple  = "org.slf4j"     %  "slf4j-simple"        % V.slf4j
-
-  // circe
-  val circeJawn    = "io.circe" %% "circe-jawn"    % V.circe
-  val circeGeneric = "io.circe" %% "circe-generic" % V.circe
 
   // doobie
   val doobieCore           = "org.tpolecat" %% "doobie-core"           % V.doobie
@@ -74,6 +65,4 @@ object Dependencies {
   val azureStorageBlob   = "com.azure"              % "azure-storage-blob"   % V.azureStorageBlob
   val azureIdentity      = "com.azure"              %  "azure-identity"      % V.azureIdentity
 
-  // transitive
-  val badRows = "com.snowplowanalytics" %% "snowplow-badrows" % V.badRows
 }
